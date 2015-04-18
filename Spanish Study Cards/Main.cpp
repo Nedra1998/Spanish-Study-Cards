31f995c2adead99dd925c4e3869348f641c7abfa
Arden Rasmussen <ardenisthebest@gmail.com>
2015-04-18 09:09:45 -0700

    Initilize

---
+++ Spanish Study Cards/Main.cpp
@@ -0,0 +1,37 @@
+#include "Libraries.h"
+#include "Hephaestus.h"
+#include "Logging.h"
+using namespace std;
+Hephaestus H;
+GLFWwindow* Win;
+void Key_Call(GLFWwindow* Win, int key, int scancode, int action, int mods){
+	H.Key_Call_Back(Win, key, scancode, action, mods);
+}
+void Mouse_Call(GLFWwindow* Win, int button, int action, int mods){
+	H.Mouse_Call_Back(Win, button, action, mods);
+}
+void Window_Call(GLFWwindow* Win, int width, int hight){
+	glfwSetWindowSize(Win, width, hight);
+}
+int main(){
+	GLFWwindow* Win;
+	Win = H.Full_Setup();
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glfwSetKeyCallback(Win, Key_Call);
+	glfwSetMouseButtonCallback(Win, Mouse_Call);
+	glfwSetWindowSizeCallback(Win, Window_Call);
+	srand(time(NULL));
+	/*>>>>>Place Before Start Code Here<<<<<*/
+
+	while (!glfwWindowShouldClose(Win)){
+		/*>>>>>Place While Running Code Here<<<<<*/
+		
+		H.Display_All_Layers();
+		H.Frame();
+	}
+	H.Terminate_Log();
+	return(0);
+}
